576341a552c0607f9283e9ad8cb84d9f6112bc49
Yolis <yolisisabel28@gmail.com>
2023-04-06 12:41:59 -0700

    created individual pages and created root as the default layout for site

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import * as ioicons from "react-icons/io5";
+import MyForm from "../components/Form";
+import Blog from "../components/Blog";
+
+const Home = () => {
+  return <div>Welcome!</div>;
+};
+
+export default Home;
