bcc73951b97958866b795a3bab467404ed5f1c85
Yolis <yolisisabel28@gmail.com>
2023-04-06 12:38:06 -0700

    created router var to define routes and called RouterProvider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,47 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MyNavBar from "./components/Navbar";
-import ListBlogs from "./components/ListBlogs";
-//import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import Home from "./pages/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AboutMe from "./pages/aboutme";
+import Root from "./pages/root";
+import Blogs from "./pages/Blogs";
 //Browser router which will actually help connect to the browser
 //routes component which is going to be the parent for all our routes
 //route, used to set up a single page
 
-function App() {
-  return (
-    <div className="App">
-      <MyNavBar />
-      <ListBlogs />
-    </div>
-  );
-}
+//root as base component
+//you have two children which is the two pages you want
+//want displayed
 
+//index: this component has no other paths, so just inherit path from
+//the parent
+
+//var router holds our routes that's being defined
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      { index: true, element: <Home /> },
+      {
+        path: "aboutme",
+        element: <AboutMe />,
+      },
+      {
+        path: "blogs",
+        element: <Blogs />,
+      },
+    ],
+  },
+]);
+
+//RouterProvider
+//figuring out what component to load based on url
+//ex. if you have '/aboutme' it will figure out you
+//would want aboutme component to display
+
+function App() {
+  return <RouterProvider router={router} />;
+}
 export default App;
